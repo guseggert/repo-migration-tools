63521c344bf64879399ecb1d9f6ce0cc1e689310
Gus Eggert <gus@gus.dev>
2022-11-14 13:12:02 -0500

    Rewrite in Python, update commit msgs, and add issue transfers

--- README.md
+++ README.md
@@ -1,91 +1,33 @@
-This is a script for migrating files from one repo to other repo, while preserving the commit history.
+This contains tools for migrating files and issues from one GitHub repo to another, while preserving the commit history.
 
-There is also a script for moving GitHub issues from one repo to another.
+## Prerequisites
+
+- Install the GitHub CLI and login using `gh auth login`
+- Install Python 3.11
+- Install pipenv: `pip install --user pipenv`
+- Clone this repo, `cd` to it, and activate the virtualenv: `pipenv shell`
 
 ## Migrating code
-### Prerequisites
 
-- Install filter-repo https://github.com/newren/git-filter-repo
+See also the help using `python migrate.py repo --help`.
 
-### Usage
+This will move files match by the globs from the source repo into a subdirectory in the destination repo. This happens locally, and the command will print the local directory of the destination repo so that the change can be inspected, pushed to a branch, and then a PR opened.
 
-First, clone both repos locally. Do not reuse existing clones--they should be new, clean clones.
-
-The result of these commands will be a new branch on the target repo containing the changes. You should then open a PR to merge the branch into main/master, using `git merge --allow-unrelated-histories`.
-
-Note that this does not migrate tags.
-
-For non-trivial migrations there may be merge conflicts. If that happens, the script will abort and leave the rebase active. Resolve the conflicts and continue the rebase until it finishes.
-
-#### Copy a repo to a subdir of another repo
-`./migrate.sh move-repo <src_repo_path> <src_repo_branch> <dest_repo_path> <dest_repo_branch>`
-
-Example: suppose I want to move all files from the master branch of repo `test/a` into the root of repo `test/b`, on a new branch called `consolidate-things`:
+This also adds a note to the bottom of each git commit message indicating that the commit was moved, and from where. For merge commits, the message is also updated so that it points to the original repo, so that the GitHub link continues to work.
 
 ```
-$ tree test
-test
-├── a
-│   └── file-a
-└── b
-    └── file-b
-
-$ ./migrate.sh move-repo test/a master test/b consolidate-things
-
-$ tree test
-test
-├── a
-│   └── file-a
-└── b
-    ├── file-a
-    └── file-b
-
+python migrate.py repo \
+  --source-repo ipfs/tar-utils \
+  --glob '*.go' --glob 'README.md' \
+  --dest-repo ipfs/libkubo \
+  --dest-subdir target \
+  --dest-branch migrate-tar-utils
 ```
 
-#### Copy a glob of files from a repo to a subdir of another repo
-`./migrate.sh move-glob-to-subdir <src_repo_path> <src_repo_branch> <src_glob> <dest_repo_path> <dest_repo_branch> <dest_subdir>`
-
-Suppose I want to move all files matching a pattern `dir-a/*.go` from the master branch of repo `test/a` into a subdirectory `from-a` of repo `test/b`, on a new branch called `consolidate-things`:
+## Migrating issues
 
 ```
-$ tree test
-test
-├── a
-│   ├── dir-a
-│   │   └── a.go
-│   └── file-a
-└── b
-    └── file-b
-
-$ ./migrate.sh move-glob-to-subdir test/a master 'dir-a/*.go' test/b consolidate-things from-a
-
-$ tree test
-test
-├── a
-│   ├── dir-a
-│   │   └── a.go
-│   └── file-a
-└── b
-    ├── file-b
-    └── from-a
-        └── dir-a
-            └── a.go
+python migrate.py issues \
+  --source-repo ipfs/tar-utils \
+  --dest-repo ipfs/libkubo
 ```
-
-## Migrating Issues
-### Prerequisites
-- `jq`
-- GitHub CLI (`gh` command), with auth configured as necessary
-
-### Usage
-Repos are slways of the form `owner/name`, e.g. if the URL is `github.com/foo/bar` then the repo you pass should be `foo/bar`.
-
-To migrate issues:
-```
-cd src_repo
-migrate-issues.sh <dest_repo>
-```
-
-This will move all issues from `src_repo` into `dest_repo` and rename them to begin with `src_repo`, like `owner/name: Some Issue`.
-
-You can dryrun a transfer to see what the result will be, without actually executing it, by setting the `DRYRUN` environment variable.
---
+++ migrate.py
@@ -0,0 +1,169 @@
+import re
+import github
+import git_filter_repo as fr
+import click
+from github.Repository import Repository
+import tempfile
+import os
+import subprocess
+import pathlib
+import urllib3
+import datetime
+import requests
+
+def run(args, wd=None):
+    if not wd:
+        wd = os.getcwd()
+    res = subprocess.run(args, cwd=wd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if res.returncode != 0:
+        raise Exception(f'error running {args[0]}: {res.stdout.decode()} {res.stderr.decode()}')
+    return res.stdout.decode().strip()
+
+class RateLimitRetry(urllib3.util.retry.Retry):
+    def get_retry_after(self, response):
+        reset_time = datetime.datetime.fromtimestamp(int(response.headers["X-RateLimit-Reset"]))
+        retry_after = (reset_time - datetime.datetime.now()).seconds + 1
+        print(f"Rate limited, retrying after {retry_after} seconds")
+        return retry_after
+
+def transfer_issue(auth_token, issue_id, dest_repo_id):
+        query = {'query': """
+        mutation{
+         transferIssue(input:{issueId:"%s",repositoryId:"%s"}) {
+          issue {
+           number
+           url
+          }
+         }
+        }
+        """ % (issue_id, dest_repo_id)}
+        return requests.post(
+            'https://api.github.com/graphql',
+            json=query,
+            headers={'Authorization': f'Bearer {auth_token}'}
+        ).json()['data']['transferIssue']['issue']
+
+def gh_token():
+    return run(['gh', 'auth', 'token'])
+    
+def new_gh(token):    
+    return github.Github(token, retry=RateLimitRetry())
+
+class MessageUpdater(object):
+    def __init__(self, source_repo: Repository):
+        self._source_repo = source_repo
+
+    def commit_callback(self, commit: fr.Commit, metadata):
+        msg = commit.message.decode()
+        if 'Merge pull request' in msg:
+            msg = re.sub(
+                'Merge pull request (.+) from (.*)',
+                f'Merge pull request {self._source_repo.full_name}\\1 from \\2',
+                msg,
+            )
+        original_id = commit.original_id
+        if original_id:
+            msg += f'\n\nThis commit was moved from {self._source_repo.full_name}@{original_id.decode()}'
+
+        commit.message = msg.encode()
+
+       
+def clone_repo(tmp_dir: str, repo: Repository) -> str:
+    print(f'Cloning {repo.full_name}')
+    clone_dir = pathlib.Path(tmp_dir)
+    repo_dir = clone_dir / repo.name
+    os.mkdir(repo_dir)
+    run(['git', 'clone', repo.clone_url, repo_dir])
+    print(f'Cloned {repo.full_name} to {repo_dir}')
+    return str(repo_dir)
+
+def filter_repo(msg_updater: MessageUpdater, source_repo_path: str, globs, dest_subdir: str):
+    glob_args = [arg for glob in globs for arg in ('--path-glob', glob)]
+    fr_args = []
+    os.chdir(source_repo_path)
+
+    if dest_subdir:
+        fr_args += ['--to-subdirectory-filter', dest_subdir]
+        
+    fr_args += glob_args
+
+    print(f'running with args: {fr_args}')
+
+    args = fr.FilteringOptions.parse_args(fr_args)
+    repo_filter = fr.RepoFilter(args, commit_callback=msg_updater.commit_callback)
+    repo_filter.run()
+
+
+def migrate_repo(gh: github.Github, tmp_dir: str, source_repo, source_branch: str, globs: list[str], dest_repo, dest_subdir: str, dest_branch):
+        source_gh_repo = gh.get_repo(source_repo)
+        dest_gh_repo = gh.get_repo(dest_repo)
+        
+        if not source_branch:
+            source_branch = source_gh_repo.default_branch
+            
+        source_repo_dir = clone_repo(tmp_dir, source_gh_repo)
+        dest_repo_dir = clone_repo(tmp_dir, dest_gh_repo)
+
+        msg_updater = MessageUpdater(source_gh_repo)
+        
+        filter_repo(msg_updater, source_repo_dir, globs, dest_subdir)
+
+        run(['git', 'remote', 'add', 'src-repo', source_repo_dir], wd=dest_repo_dir)
+        run(['git', 'checkout', '-B', 'tmp-migrate-branch'], wd=dest_repo_dir)
+        run(['git', 'pull', '--allow-unrelated-histories', '--no-rebase', 'src-repo', source_branch], wd=dest_repo_dir)
+        run(['git', 'checkout', '-B', dest_branch], wd=dest_repo_dir)
+        run(['git', 'merge', 'tmp-migrate-branch'], wd=dest_repo_dir)
+        run(['git', 'commit', '--amend', '-m', f'Merge commits from {source_repo}/{source_branch}'], wd=dest_repo_dir)
+
+        return dest_repo_dir
+
+
+@click.command(name='repo', help='''Move a set of files/dirs from one GitHub repo to another into a subdirectory, preserving the history.
+
+It's recommended to transfer into a new subdirectory to avoid having to deal with conflicts, and then refactoring the directory structure in a separate PR afterwards.
+
+Some commit messages are rewritten so that GitHub links will continue to work correctly, such as links to pull requests in merge commits. Each commit message will also have a note added to the bottom of it explaining that the commit was transferred, and from where. 
+
+This requires an installed and configured GitHub CLI, see https://cli.github.com/.
+''')
+@click.option('--source-repo', required=True, help='the source repo, in the form <owner>/<name>, such as "ipfs/kubo"')
+@click.option('--source-branch', help='the source repo branch to use, defaults to the GitHub master branch (usually "master" or "main")')
+@click.option('--glob', required=True, multiple=True, help='glob indicating the set of dirs and files in the source repo to move to the destination repo; can be specified multiple times')
+@click.option('--dest-repo', required=True, help='the destination repo, in the form <owner>/<name>, such as "ipfs/kubo"')
+@click.option('--dest-subdir', help='the relative subdirectory in the destination repo to place the files from the source repo')
+@click.option('--dest-branch', required=True, help='the branch to create in the destination repo to contain the changes')
+def migrate_repo_cmd(source_repo, source_branch, glob, dest_repo, dest_subdir, dest_branch):
+    globs = list(glob)
+    gh = new_gh(gh_token())
+
+    tmp_dir = tempfile.mkdtemp()
+    dest_repo_dir = migrate_repo(gh, tmp_dir, source_repo, source_branch, globs, dest_repo, dest_subdir, dest_branch)
+    print(f'\n\nWork done in repo: {dest_repo_dir}')
+    print('Switch to that directory, finish the merge if necessary, and then open a pull request.')
+
+    
+@click.command(name='issues', help='Migrate issues from one repo to another.')
+@click.option('--source-repo', required=True)
+@click.option('--dest-repo', required=True)
+def migrate_issues_cmd(source_repo, dest_repo):
+    token = gh_token()
+    gh = new_gh(token)
+    gh_repo = gh.get_repo(dest_repo)
+    repo_id = gh_repo.raw_data['node_id']
+    
+    issues = gh.search_issues(f'is:issue repo:{source_repo}')
+    for issue in issues:
+        issue_id = issue.raw_data['node_id']
+        new_issue = transfer_issue(token, issue_id, repo_id)
+        new_issue_url = new_issue['url']
+        print(f'Transferred issue from {issue.url} to {new_issue_url}')
+            
+    
+@click.group()
+def migrate():
+    pass
+
+migrate.add_command(migrate_repo_cmd)
+migrate.add_command(migrate_issues_cmd)
+    
+migrate()
